ef9d23c94366c44d6113e4a24b4c011e70981a8b
6toos <chukofsi@gmail.com>
2024-05-31 04:30:52 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const p = React.createElement("p", {}, "This is a paragraph tag!");
 const p2 = React.createElement("p", {}, "This is a paragraph tag!");
-const div = React.createElement("div", { className: "name" }, [p, p2]);
+// const div = React.createElement("div", { className: "name" }, [p, p2]);
+const div = <div className='name'>This is a JXS</div>
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(div);
