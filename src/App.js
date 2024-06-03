236c45edf34532c0d9244b7357f1261d794da5c8
6toos <chukofsi@gmail.com>
2024-06-03 03:49:45 +0100

    map function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "./Button";
+import Text from "./Text";
 
 const App = () => {
-    const ClickHandler = () => {
-        console.log("I was clicked from the parent...")
+    const { data, setData } = useState([
+        { id: 1, test: "test one" },
+        { id: 2, test: "test two" },
+        { id: 3, test: "test three" },
+        { id: 4, test: "test four" },
+        { id: 5, test: "test five" }
+    ]);
+    const updateData = () => {
+        setData((nextData) => [{ id: 6, test: "test six" }, ...nextData])
     }
-    return <Button clickAction={ClickHandler}>Click me</Button>;
+    return
+        <>
+        {data.map((item, index) => <Text key={index}>{ item.test}</Text>)}
+        <Button clickAction={updateData}>Add more...</Button>;
+    </>
 }
 
 export default App;
